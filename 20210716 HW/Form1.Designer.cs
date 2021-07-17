77ae71deefa801d85f4983686deac6405d3dbcc1
hellotherepcw <jimmy50916@gmail.com>
2021-07-17 12:26:04 +0800

    加入專案檔案。

---
+++ 20210716 HW/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace _20210716_HW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.outputTextBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(126, 50);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 16);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(210, 50);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 16);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.outputTextBox1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.dateTimePicker2);
+            this.panel1.Controls.Add(this.dateTimePicker1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.comboBox2);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Location = new System.Drawing.Point(109, 38);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(553, 358);
+            this.panel1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(126, 164);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(169, 20);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(126, 114);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(169, 20);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(182, 234);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(286, 234);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(327, 24);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(327, 61);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(358, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "label3";
+            // 
+            // outputTextBox1
+            // 
+            this.outputTextBox1.Location = new System.Drawing.Point(59, 263);
+            this.outputTextBox1.Multiline = true;
+            this.outputTextBox1.Name = "outputTextBox1";
+            this.outputTextBox1.Size = new System.Drawing.Size(450, 73);
+            this.outputTextBox1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox outputTextBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 20210716 HW/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace _20210716_HW
+{
+    public partial class Form1 : Form
+    {
+        Hashtable selfCarCC = new Hashtable()
+        {
+            {"<500cc" , 1620 },
+            {"501cc - 600cc"  , 2160    },
+            {"601cc - 1200cc" , 4320    },
+            {"1201cc - 1800cc", 7120    },
+            {"1801cc - 2400cc" ,11230   },
+            {"2401cc - 3000cc", 15210   },
+            {"3001cc - 4200cc" ,28220   },
+            {"4201cc - 5400cc" ,46170   },
+            {"5401cc - 6600cc" ,69690   },
+            {"6601cc - 7800cc" ,117000  },
+            {"7801cc ",   151200        },
+        };
+        Hashtable businessCarCC = new Hashtable()
+        {
+            {"<500cc",  900 },
+            {"501cc - 600cc",  1260},
+            {"601cc - 1200cc",  2160},
+            {"1201cc - 1800cc", 3060},
+            {"1801cc - 2400cc", 6480},
+            {"2401cc - 3000cc", 9900},
+            {"3001cc - 4200cc", 16380},
+            {"4201cc - 5400cc", 24300},
+            {"5401cc - 6600cc", 33660},
+            {"6601cc - 7800cc", 44460},
+            {">7801cc",   56700},
+        };
+
+        Hashtable coachCarCC = new Hashtable()
+        {
+            {"501cc - 600cc",   1080},
+            {"601cc - 1200cc",  1800},
+            {"1201cc - 1800cc", 2700},
+            {"1801cc - 2400cc", 3600},
+            {"2401cc - 3000cc", 4500},
+            {"3001cc~3600cc",   5400},
+            {"3601cc~4200cc",   6300},
+            {"4201cc~4800cc",   7200},
+            {"4801cc~5400cc",   8100},
+            {"5401cc~6000cc",   9000},
+            {"6001cc~6600cc",   9900},
+            {"6601cc~7200cc",   1080},
+            {"7201cc~7800cc",   1170},
+            {"7801cc~8400cc",   1260},
+            {"8401cc~9000cc",   1350},
+            {"9001cc~9600cc",   1440},
+            {"9601cc~10200cc",  1530},
+            {">10201cc",  16200     },
+        };
+        Hashtable truckCC = new Hashtable()
+        {
+            {"<500cc",     900 },
+            {"501cc - 600cc",   1080},
+            {"601cc - 1200cc",  1800},
+            {"1201cc - 1800cc", 2700},
+            {"1801cc - 2400cc", 3600},
+            {"2401cc - 3000cc", 4500},
+            {"3001cc~3600cc",   5400},
+            {"3601cc~4200cc",   6300},
+            {"4201cc~4800cc",   7200},
+            {"4801cc~5400cc",   8100},
+            {"5401cc~6000cc",   9000},
+            {"6001cc~6600cc",   9900},
+            {"6601cc~7200cc",   10800},
+            {"7201cc~7800cc",   11700},
+            {"7801cc~8400cc",   12600},
+            {"8401cc~9000cc",   13500},
+            {"9001cc~9600cc",   14400},
+            {"9601cc~10200cc",  15300},
+            {">10201cc",   16200},
+        };
+
+        Hashtable motorcycleCC = new Hashtable()
+        {
+            {"<150cc",    0},
+            {"151cc~ 250cc",    800},
+            {"251cc~ 500cc",    1620},
+            {"501cc~ 600cc",    2160},
+            {"601cc~1200cc",    4320},
+            {"1201cc~1800cc",   7120},
+            {">1801cc",   11230},
+        };
+        Hashtable electriclMotoHP = new Hashtable()
+        {
+            {"<20.19HP <21.54PS", 0},
+            {"20.20-40.03HP 21.55-42.71PS", 800},
+            {"40.04-50.07HP 42.72-53.43PS", 1620},
+            {"50.08-58.79HP 53.44-62.73PS", 2160},
+            {"58.80-114.11HP    62.74-121.76PS",    4320},
+            {">114.12HP    >121.77PS",    7120},
+        };
+        Hashtable electricalCoachCarHP = new Hashtable()
+        {
+            {"<138HP   <140.1PS", 4500 },
+            {"138.1-200HP   140.2-203PS",   6300},
+            {"200.1-247HP   203.1-250.7PS", 8100},
+            {"247.1-286HP   250.8-290.3PS", 9900},
+            {"286.1-336HP   290.4-341PS",   11700},
+            {"336.1-361HP   341.1-366.4PS", 13500},
+            {">361.1HP >366.5PS", 15300},
+        };
+    public Form1()
+    {
+        InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+    }
+}
